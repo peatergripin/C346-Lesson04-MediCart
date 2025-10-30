6ddfacaf216c48434841962b1b077b7eaac87f30
irfan <peatergripin@gmail.com>
2025-10-30 10:45:58 +0800

    first commit

--- App.js
+++ App.js
@@ -4,18 +4,25 @@ import boatData from './data';
 
 const App = () => {
     return (
-        <ScrollView>
-            <StatusBar background = "gray" />
+        <ScrollView style={styles.container}>
+            <StatusBar barStyle="default"/>
             <Text style={styles.header}>GetABoat - For Sale</Text>
             {boatData.map(boat => <Boat key = {boat.name} name= {boat.name} description = {boat.description} picture={boat.picture}/>)}
 
         </ScrollView>
     );
 };
-const styles = StyleSheet.create({header:{
+const styles = StyleSheet.create({
+    header:{
     textAlign: 'center',
     marginVertical: 5,
     fontSize: 20,
     fontWeight: 'bold'
-    } });
+    } ,
+    container:{
+        paddingBottom: 5,
+    }
+
+
+});
 export default App;
